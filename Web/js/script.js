d0b70c3c7a33b79b8d24ea76a81c97839ed833c5
Latsari <latsari@hotmail.com>
2016-11-14 19:52:27 +0100

    Update script.js

--- Web/js/script.js
+++ Web/js/script.js
@@ -11,10 +11,21 @@ $(function() {
       }
     }
   });
-});
 
-function revealOnScroll() {
-    var scrolled = $window.scrollTop();
+  // Animación
+  var $window           = $(window),
+      win_height_padded = $window.height() * 1.1,
+      isTouch           = Modernizr.touch;
+
+  if (isTouch) { $('.revealOnScroll').addClass('animated'); }
+
+  $window.on('scroll', revealOnScroll);
+
+  function revealOnScroll() {
+    var scrolled = $window.scrollTop(),
+        win_height_padded = $window.height() * 1.1;
+
+    // Showed...
     $(".revealOnScroll:not(.animated)").each(function () {
       var $this     = $(this),
           offsetTop = $this.offset().top;
@@ -29,3 +40,15 @@ function revealOnScroll() {
         }
       }
     });
+    // Hidden...
+   $(".revealOnScroll.animated").each(function (index) {
+      var $this     = $(this),
+          offsetTop = $this.offset().top;
+      if (scrolled + win_height_padded < offsetTop) {
+        $(this).removeClass('animated fadeInUp flipInX lightSpeedIn')
+      }
+    });
+  }
+
+  revealOnScroll();
+});
