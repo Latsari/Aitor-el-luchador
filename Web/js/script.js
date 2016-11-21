4d5d70c5a3901ee407fc6de28dcddfee732f46a5
edual16 <eduardo.alfaro@outlook.com>
2016-11-21 19:55:07 +0100

    Update script.js

--- Web/js/script.js
+++ Web/js/script.js
@@ -13,26 +13,6 @@ $(document).ready(function() {
     }
 });
   // end nav
-    
-    $('.sldr').each(function() {
-        var th = $(this);
-        th.sldr({
-            focalClass    : 'focalPoint',
-            offset        : th.width() / 2,
-            sldrWidth     : 'responsive',
-            nextSlide     : th.nextAll( '.sldr-nav.next:first' ),
-            previousSlide : th.nextAll( '.sldr-nav.prev:first' ),
-            selectors     : th.nextAll( '.selectors:first' ).find( 'li' ),
-            toggle        : th.nextAll( '.captions:first' ).find( 'div' ),
-            /*sldrInit      : function() {},
-            sldrStart     : function() {},
-            sldrComplete  : function() {},
-            sldrLoaded    : function() {},*/
-            sldrAuto      : true,
-            sldrTime      : 2000,
-            hasChange     : true
-        });
-    });
   
   $('.parallax-window-1').parallax({imageSrc: 'images/Aitor1.jpg'});
   
