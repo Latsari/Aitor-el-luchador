fc6ff01ef1b71c63d50e37c0a29aa48997c49050
edual16 <eduardo.alfaro@outlook.com>
2016-11-21 16:22:25 +0100

    Update script.js

--- Web/js/script.js
+++ Web/js/script.js
@@ -1,3 +1,27 @@
+$( window ).load( function() {
+
+    $( '.sldr' ).each( function() {
+        var th = $( this );
+        th.sldr({
+            focalClass    : 'focalPoint',
+            offset        : th.width() / 2,
+            sldrWidth     : 'responsive',
+            nextSlide     : th.nextAll( '.sldr-nav.next:first' ),
+            previousSlide : th.nextAll( '.sldr-nav.prev:first' ),
+            selectors     : th.nextAll( '.selectors:first' ).find( 'li' ),
+            toggle        : th.nextAll( '.captions:first' ).find( 'div' ),
+            sldrInit      : sldrInit,
+            sldrStart     : sldrStart,
+            sldrComplete  : sldrComplete,
+            sldrLoaded    : sldrLoaded,
+            sldrAuto      : true,
+            sldrTime      : 2000,
+            hasChange     : true
+        });
+    });
+
+});
+
 $(document).ready(function() {
   
   // nav
