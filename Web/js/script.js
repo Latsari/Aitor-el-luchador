30ee88ad0a9c68a0e458db6246370ebc3951a272
edual16 <eduardo.alfaro@outlook.com>
2016-12-26 11:00:25 +0100

    Update script.js

--- Web/js/script.js
+++ Web/js/script.js
@@ -16,6 +16,7 @@ $(document).ready(function() {
       }
      });
   
+  /*
   $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
@@ -28,6 +29,7 @@ $(document).ready(function() {
       }
     }
   });
+  */
   
   // Animación
   var $window           = $(window),
