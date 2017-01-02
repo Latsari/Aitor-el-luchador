4f6824c6b81f9abf28a145781de75ba57f76101c
edual16 <eduardo.alfaro@outlook.com>
2017-01-02 17:53:42 +0100

    Update script.js

--- Web/js/script.js
+++ Web/js/script.js
@@ -28,23 +28,23 @@ $(document).ready(function() {
 
           // Using jQuery's animate() method to add smooth page scroll
           // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-        if ( $("nav").hasClass("affix")) {
+        //if ( $("nav").hasClass("affix")) {
           $('html, body').animate({
-            scrollTop: $(hash).offset().top - 51
-          }, 800, function(){
+            scrollTop: $(hash).offset().top// - 51
+         // }, 800, function(){
 
           // Add hash (#) to URL when done scrolling (default click behavior)
             window.location.hash = hash;
           });
-      } else {
-          $('html, body').animate({
-            scrollTop: $(hash).offset().top - 120
-          }, 800, function(){
+      //} else {
+      //    $('html, body').animate({
+      //      scrollTop: $(hash).offset().top - 120
+      //    }, 800, function(){
 
           // Add hash (#) to URL when done scrolling (default click behavior)
-            window.location.hash = hash;
-          });
-      }
+      //      window.location.hash = hash;
+      //    });
+      //}
 
         } // End if
       
